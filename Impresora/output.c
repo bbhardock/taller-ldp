0d7f0e40dd110ea14e0e55b897d56441c54e294a
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-26 18:01:57 -0300

    Cambiado programa mostrar

--- Impresora/output.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int main() {
-Chait dijo que la variable nombre_bueno se declaro 
-Chait dijo que la variable nombre_malo se declaro 
-Chait dice que se va a imprimir: nombre_malonombre_bueno junto con nombre_malo"Hola soy" junto con nombre_buenoreturn 0;
-}
