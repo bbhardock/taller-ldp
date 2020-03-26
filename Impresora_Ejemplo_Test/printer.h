cb39d7f3539199c7eed1b63b7aef5646f28a5b40
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-26 16:36:21 -0300

    Cambiado nombre impresora

---
+++ Impresora_Ejemplo_Test/printer.h
@@ -0,0 +1,9 @@
+void inicio ();
+void fin ();
+void leyendoTipoString (int);
+void CrearVariable (char*,char*);
+void concatenaOperacion (char*,char*,char*,char*);
+void RedefiniendoVariable (char*,char*);
+void imprimir (char*);
+void imprimirTexto (char*, char*);
+void imprimirVariable (char*, char*);
