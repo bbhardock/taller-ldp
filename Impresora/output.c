2f7f78960297d8aa80f940463e343931450fedea
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-26 18:20:08 -0300

    fixed para output

--- Impresora/output.c
+++ Impresora/output.c
@@ -4,7 +4,7 @@
 int main() {
 	char nombre_bueno[2048] = "Marcos Chait";
 	char nombre_malo[2048] = "Charcos Mait";
-	printf("%s%s%s", "Hola soy ", nombre_bueno, nombre_malo);
+	printf("%s%s%s%s", "Hola soy ", nombre_bueno, " no ", nombre_malo);
 
  return 0;
 }
