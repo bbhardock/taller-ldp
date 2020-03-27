64a30e8574e1feb7046053a7893bf2ee8131a780
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-26 21:36:17 -0300

    Cambiado programa 04

--- Impresora/output.c
+++ Impresora/output.c
@@ -4,8 +4,10 @@
 int main() {
 	int variable_uno = 1;
 	int variable_dos = 2;
+	int variable_3 = (3*2);
 	int suma = variable_uno+variable_dos*(6/3);
-	printf("%i", suma);
+	int suma_final = variable_3+suma;
+	printf("%i", suma_final);
 
  return 0;
 }
