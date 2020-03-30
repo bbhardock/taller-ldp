f39627695b12b23e83b16d0a00d81f1b93252ab4
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-29 22:27:54 -0300

    Agregado programa de calculo factorial

--- Impresora/output.c
+++ Impresora/output.c
@@ -2,23 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-	char chait[2048] = "Marcos ";
-	char marcos[2048] = "CHAIT";
-	char prueba[2048] = "tOdo maYuscula 69";
-	strcat(chait,marcos);
-	printf("%s%s", chait, "\n");
-	printf("%s%s", prueba, "\n");
+	int n = 2;
+	int factorial = 1;
 
-	for(int i = 0; i < 2048; i++){
-		if(prueba[i] != 0){
-			for(int j = 97;j <= 122;j++){
-				if(prueba[i] == j){
-					prueba[i] = j-32;
-				}
-			}
-		}else{break;}
+	while (n>0){
+	factorial = factorial*n;
+	n--;
 	}
-	printf("%s%s", prueba, "\n");
+	printf("%d", factorial);
 
  return 0;
 }
