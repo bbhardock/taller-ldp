29a6975a30cb9066a2b30bfaa5e137ee54b0e42b
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-03-30 18:55:48 -0300

    fg

---
+++ Impresora/output.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+int main() {
+	int variable_uno = 1;
+	int variable_dos = 2;
+	int variable_tres = 3;
+	int variable_cuatro = 4;
+	int variable_3 = (variable_uno+variable_dos+variable_tres)/variable_cuatro;
+	int suma = variable_uno+variable_dos*(6/3);
+	int suma_final = variable_3+suma;
+	printf("valor : %d \n", suma_final);
+
+ return 0;
+}
