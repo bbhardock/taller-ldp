a9ceb7f569f4f13f1bae0092019978518aa6f3d5
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-30 17:00:36 -0300

    Agregado matematicas

--- Impresora/output.c
+++ Impresora/output.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <stdbool.h>
 int main() {
-	int variable_1 = 1;
-	char variable_2[2048] = "si";
-
-	if (variable_1>0){
-	variable_1 = 2*(3+3);
-	}
-	printf("%d", variable_1);
+	int variable_uno = 1;
+	int variable_dos = 2;
+	int variable_tres = 3;
+	int variable_3 = (variable_uno+variable_dos+variable_3)/4;
+	int suma = variable_uno+variable_dos*(6/3);
+	int suma_final = variable_3+suma;
+	printf("%d", suma_final);
 
  return 0;
 }
