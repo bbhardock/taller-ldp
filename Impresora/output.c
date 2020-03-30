cc940fd837b0c5575a23ede81be1cdbeafaa4982
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-03-30 19:01:38 -0300

    Secuencia de Fibonacci

--- Impresora/output.c
+++ Impresora/output.c
@@ -3,14 +3,21 @@
 #include <string.h>
 #include <stdbool.h>
 int main() {
-	int variable_uno = -1;
-	int variable_dos = 2;
-	int variable_tres = 3;
-	int variable_cuatro = 4;
-	int variable_3 = (variable_uno+variable_dos+variable_tres)/variable_cuatro;
-	int suma = variable_uno+variable_dos*(6/3);
-	int suma_final = variable_3+suma;
-	printf("%d", suma_final);
+	int a = 0;
+	int b = 1;
+	int n = 10;
+	n = n+1;
+	printf("%d%s", b, " ");
+	int comprobacion = n-2;
+
+	while (comprobacion>0){
+	int c = a+b;
+	a = b;
+	b = c;
+	printf("%d%s", c, " ");
+	n = n-1;
+	comprobacion = n-2;
+	}
 
  return 0;
 }
