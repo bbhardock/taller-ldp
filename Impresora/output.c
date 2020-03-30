216ee9ce359015507be45969d388b69b537af1a6
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-03-30 17:13:41 -0300

    Update output.c

--- Impresora/output.c
+++ Impresora/output.c
@@ -4,17 +4,19 @@
 #include <stdbool.h>
 int main() {
 	int cantidad_pruebas = 3;
-	int nota_prueba_uno = 10;
-	int nota_prueba_dos = 10;
-	int nota_prueba_tres = 10;
+	int nota_prueba_uno = 50;
+	int nota_prueba_dos = 90;
+	int nota_prueba_tres = 50;
 	int promedio = (nota_prueba_uno+nota_prueba_dos+nota_prueba_tres)/cantidad_pruebas;
 	promedio = promedio/40;
 
 	if (promedio>0){
 	printf("%s", "Aprobaste");
+	printf("%d",promedio);
 	}
 	else{
 	printf("%s", "No aprobaste na mano");
+	printf("%d",promedio);
 	}
 
  return 0;
